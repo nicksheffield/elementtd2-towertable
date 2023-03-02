1794487150841ab4ec64db3ecd09a03be2c21ac4
Nick Sheffield <numbereft@gmail.com>
2023-03-02 19:52:29 +1300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,361 @@
+import { createContext, useContext, useState } from 'react'
+import { ElementName, elementNames, getBackgroundColor, getTowerParents, Range, Support, Tower, towers } from './data'
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+const isNotNull = <T,>(x: T | null): x is T => x !== null
+
+type ContextType = {
+	elements: ElementName[]
+	setElements: React.Dispatch<React.SetStateAction<ElementName[]>>
+	hovered: Tower | null
+	setHovered: React.Dispatch<React.SetStateAction<Tower | null>>
+	range: Range | null
+	setRange: React.Dispatch<React.SetStateAction<Range | null>>
+	support: Support | null
+	setSupport: React.Dispatch<React.SetStateAction<Support | null>>
+}
+
+const Context = createContext({} as ContextType)
+
+function App() {
+	const [elements, setElements] = useState<ElementName[]>([])
+	const [hovered, setHovered] = useState<Tower | null>(null)
+	const [range, setRange] = useState<Range | null>(null)
+	const [support, setSupport] = useState<Support | null>(null)
+
+	return (
+		<Context.Provider value={{ elements, setElements, hovered, setHovered, range, setRange, support, setSupport }}>
+			<Overlay />
+			<div className="flex flex-col gap-4 pt-4 h-screen">
+				<div className="flex flex-row justify-between max-w-6xl container mx-auto">
+					<div>Picks Remaining: {11 - elements.length}</div>
+					<div>
+						<button
+							className={'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium'}
+							onClick={() => setElements([])}
+						>
+							Reset
+						</button>
+					</div>
+				</div>
+				<div className="flex flex-row justify-between max-w-6xl container mx-auto">
+					<div className="flex gap-3">
+						<div>Range:</div>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									range === 750 && 'bg-red-500'
+								)
+							)}
+							onClick={() => setRange((r) => (r !== 750 ? 750 : null))}
+						>
+							750
+						</button>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									range === 900 && 'bg-red-500'
+								)
+							)}
+							onClick={() => setRange((r) => (r !== 900 ? 900 : null))}
+						>
+							900
+						</button>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									range === 1150 && 'bg-red-500'
+								)
+							)}
+							onClick={() => setRange((r) => (r !== 1150 ? 1150 : null))}
+						>
+							1150
+						</button>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									range === 1500 && 'bg-red-500'
+								)
+							)}
+							onClick={() => setRange((r) => (r !== 1500 ? 1500 : null))}
+						>
+							1500
+						</button>
+					</div>
+
+					<div className="flex gap-3">
+						<div>Support:</div>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									support === 'buff' && 'bg-green-500'
+								)
+							)}
+							onClick={() => setSupport((r) => (r !== 'buff' ? 'buff' : null))}
+						>
+							Buff
+						</button>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									support === 'slow' && 'bg-green-500'
+								)
+							)}
+							onClick={() => setSupport((r) => (r !== 'slow' ? 'slow' : null))}
+						>
+							Slow
+						</button>
+						<button
+							className={twMerge(
+								clsx(
+									'rounded-sm bg-gray-200 text-black px-2 text-sm font-medium',
+									support === 'amplify' && 'bg-green-500'
+								)
+							)}
+							onClick={() => setSupport((r) => (r !== 'amplify' ? 'amplify' : null))}
+						>
+							Amplify
+						</button>
+					</div>
+				</div>
+
+				<div className="flex flex-row justify-center flex-start h-full gap-8">
+					<div>
+						<div className="grid grid-cols-6">
+							<SectionRow
+								getTowers={(element) => towers[element].filter((x) => x.elements.length === 1)}
+							/>
+							<SectionRow
+								getTowers={(element) => towers[element].filter((x) => x.elements.length === 2)}
+							/>
+							<SectionRow
+								getTowers={(element) => towers[element].filter((x) => x.elements.length === 3)}
+							/>
+							<SectionRow
+								getTowers={(element) => towers[element].filter((x) => x.elements.length === 4)}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Context.Provider>
+	)
+}
+
+const TowerSelector = ({ tower }: { tower: Tower }) => {
+	const { elements, setElements, hovered, setHovered, range, support } = useContext(Context)
+
+	const isActive = tower.elements.reduce((acc, cur) => acc && elements.includes(cur), true)
+
+	const level = Math.min(
+		...elementNames.filter((x) => tower.elements.includes(x)).map((x) => elements.filter((y) => y === x).length)
+	)
+
+	const correctLevel = (level: number) => Math.min(level, 5 - tower.elements.length)
+
+	const increase = () => {
+		if (tower.elements.length === 1) {
+			if (elements.length >= 11) return
+			if (elements.filter((x) => x === tower.elements[0]).length >= 3) return
+			setElements((e) => [...e, tower.elements[0]])
+		} else {
+			const targetLevel = correctLevel(level) + 1
+			const maxLevel = correctLevel(5)
+			const elementsToAdd: ElementName[] = []
+
+			if (targetLevel > maxLevel) return
+
+			tower.elements.forEach((element) => {
+				let currentCount = elements.filter((x) => x === element).length
+
+				if (targetLevel - currentCount < 0) return
+				for (let i = 0; i < targetLevel - currentCount; i++) {
+					elementsToAdd.push(element)
+				}
+			})
+
+			if (elements.length + elementsToAdd.length > 11) return
+			setElements((e) => [...e, ...elementsToAdd])
+		}
+	}
+
+	const decrease = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		e.preventDefault()
+
+		if (tower.elements.length === 1) {
+			setElements((e) => {
+				let lastIndex = e.lastIndexOf(tower.elements[0])
+				if (lastIndex === -1) return e
+				return [...e.slice(0, lastIndex), ...e.slice(lastIndex + 1)]
+			})
+		} else {
+			setElements((e) => {
+				let clone = [...e]
+				tower.elements.forEach((x) => {
+					clone.splice(clone.lastIndexOf(x), 1)
+				})
+
+				return clone
+			})
+		}
+	}
+
+	return (
+		<div
+			className="relative flex flex-col items-center w-16 select-none transition-transform scale-100 hover:scale-105 cursor-pointer"
+			onClick={increase}
+			onContextMenu={decrease}
+			onMouseOver={() => setHovered(tower)}
+			onMouseOut={() => setHovered(null)}
+		>
+			<div className="text-xs font-medium whitespace-nowrap">{tower.name}</div>
+			{/* {elements.filter((x) => x === element).length || ''} */}
+			<div
+				className={twMerge(
+					clsx(
+						tower.range === range && 'ring-4 ring-red-500',
+						tower.support === support && 'ring-4 ring-green-500',
+						tower.range === range &&
+							tower.support === support &&
+							'ring-4 ring-offset-4 ring-red-500 ring-offset-green-500',
+						tower.elements.length === 1 ? 'rounded-full' : 'border-4 border-black'
+						// tower.elements.length === 1 &&
+						// 	hovered &&
+						// 	hovered !== tower &&
+						// 	hovered.elements.includes(tower.elements[0]) &&
+						// 	'ring-4 ring-white'
+					)
+				)}
+			>
+				<img
+					className={`${!isActive && tower.elements.length !== 1 ? 'saturate-0 opacity-30' : ''}`}
+					src={tower.image}
+					data-tower={tower.name}
+				/>
+			</div>
+			<div className="absolute bottom-0 left-0 w-full flex justify-center text-3xl font-bold text-white">
+				<div
+					style={{
+						textShadow:
+							'-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000',
+					}}
+				>
+					{correctLevel(level) || ''}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+const SectionRow = ({ getTowers }: { getTowers: (element: ElementName) => Tower[] }) => {
+	return (
+		<>
+			{(Object.keys(towers) as ElementName[]).map((element) => (
+				<div
+					key={element}
+					className={`flex flex-row items-start flex-wrap justify-center content-start gap-4 w-48 p-4 ${getBackgroundColor(
+						element
+					)}`}
+				>
+					{getTowers(element).map((tower) => (
+						<TowerSelector key={tower.name} tower={tower} />
+					))}
+				</div>
+			))}
+		</>
+	)
+}
+
+type Line = {
+	x1: number
+	y1: number
+	x2: number
+	y2: number
+	depth: number
+}
+
+const createLine = (el1: HTMLImageElement, el2: HTMLImageElement, depth: number) => {
+	let rect1 = el1.getBoundingClientRect()
+	let rect2 = el2.getBoundingClientRect()
+
+	return {
+		x1: rect1.x + rect1.width / 2,
+		y1: rect1.y + rect1.height / 2,
+		x2: rect2.x + rect2.width / 2,
+		y2: rect2.y + rect2.height / 2,
+		depth,
+	}
+}
+
+const Overlay = () => {
+	const { hovered } = useContext(Context)
+
+	function getLines(tower: Tower, depth: number, lines: Line[] = []): Line[] {
+		let el = document.querySelector(`[data-tower="${tower.name}"]`) as HTMLImageElement
+		let parents = getTowerParents(tower)
+		let parentEls = parents.map((x) => document.querySelector(`[data-tower="${x.name}"]`) as HTMLImageElement)
+
+		for (let i = 0; i < parentEls.filter(Boolean).length; i++) {
+			const line = createLine(el, parentEls[i], depth)
+
+			if (!lines.find((x) => x.x1 === line.x1 && x.x2 === line.x2 && x.y1 === line.y1 && x.y2 === line.y2)) {
+				lines.push(line)
+			}
+
+			let parentTower = Object.values(towers)
+				.flat()
+				.find((x) => x.name === parentEls[i].dataset.tower)
+
+			if (parentTower) {
+				const parentTowerParents = getTowerParents(parentTower)
+
+				if (parentTowerParents.length > 0) {
+					getLines(parentTower, depth + 1, lines)
+				}
+			}
+		}
+
+		// let childTowers = Object.values(towers)
+		// 	.flat()
+		// 	.filter((x) => x.parents.includes(tower.name))
+
+		// for (let i = 0; i < childTowers.length; i++) {
+		// 	getLines(childTowers[i], lines)
+		// }
+
+		return lines
+	}
+
+	const lines: (Line | null)[] = hovered ? getLines(hovered, 0) : []
+
+	return (
+		<svg
+			className="fixed z-10 top-0 left-0 w-screen h-screen pointer-events-none fill-none bg-transparent"
+			viewBox={`0 0 ${window.innerWidth} ${window.innerHeight}`}
+		>
+			{lines.filter(isNotNull).map((line, i) => (
+				<line
+					key={i}
+					x1={line.x1}
+					x2={line.x2}
+					y1={line.y1}
+					y2={line.y2}
+					// strokeWidth={4 - line.depth}
+					style={{ opacity: 1 - line.depth * 0.33 }}
+					stroke="white"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			))}
+		</svg>
+	)
+}
+
+export default App
---
+++ src/data.ts
@@ -0,0 +1,481 @@
+import Light from './assets/light/Light.webp'
+import AtomTower from './assets/light/Atom_Tower.webp'
+import LightningTower from './assets/light/Lightning_Tower.webp'
+import TrickeryTower from './assets/light/Trickery_Tower.webp'
+import AstralTower from './assets/light/Astral_Tower.webp'
+import LaserTower from './assets/light/Laser_Tower.webp'
+import NovaTower from './assets/light/Nova_Tower.webp'
+import RailgunTower from './assets/light/Railgun_Tower.webp'
+import SingularityTower from './assets/light/Singularity_Tower.webp'
+
+import Darkness from './assets/dark/Darkness.webp'
+import PoisonTower from './assets/dark/Poison_Tower.webp'
+import DiseaseTower from './assets/dark/Disease_Tower.webp'
+import RunicTower from './assets/dark/Runic_Tower.webp'
+import EtherealTower from './assets/dark/Ethereal_Tower.webp'
+import JinxTower from './assets/dark/Jinx_Tower.webp'
+import PlagueTower from './assets/dark/Plague_Tower.webp'
+import DoomTower from './assets/dark/Doom_Tower.webp'
+import PhantomZoneTower from './assets/dark/PhantomZone_Tower.webp'
+
+import Water from './assets/water/Water.webp'
+import VaporTower from './assets/water/Vapor_Tower.webp'
+import IceTower from './assets/water/Ice_Tower.webp'
+import WellTower from './assets/water/Well_Tower.webp'
+import FloodingTower from './assets/water/Flooding_Tower.webp'
+import WispTower from './assets/water/Wisp_Tower.webp'
+import WindstormTower from './assets/water/Windstorm_Tower.webp'
+import PolarTower from './assets/water/Polar_Tower.webp'
+import TsunamiTower from './assets/water/Tsunami_Tower.webp'
+import CrystalSpireTower from './assets/water/CrystalSpire_Tower.webp'
+
+import Fire from './assets/fire/Fire.webp'
+import SolarTower from './assets/fire/Solar_Tower.webp'
+import InfernalTower from './assets/fire/Infernal_Tower.webp'
+import BlacksmithTower from './assets/fire/Blacksmith_Tower.webp'
+import FlamethrowerTower from './assets/fire/Flamethrower_Tower.webp'
+import HasteTower from './assets/fire/Haste_Tower.webp'
+import CorrosionTower from './assets/fire/Corrosion_Tower.webp'
+import ObeliskTower from './assets/fire/Obelisk_Tower.webp'
+import RageTower from './assets/fire/Rage_Tower.webp'
+
+import Nature from './assets/nature/Nature.webp'
+import MushroomTower from './assets/nature/Mushroom_Tower.webp'
+import BloomTower from './assets/nature/Bloom_Tower.webp'
+import ImpulseTower from './assets/nature/Impulse_Tower.webp'
+import GolemTower from './assets/nature/Golem_Tower.webp'
+import RootTower from './assets/nature/Root_Tower.webp'
+import IncantationTower from './assets/nature/Incantation_Tower.webp'
+import ArchdruidTower from './assets/nature/Archdruid_Tower.webp'
+import TeslaTreeTower from './assets/nature/TeslaTree_Tower.webp'
+import LifeAltarTower from './assets/nature/LifeAltar_Tower.webp'
+
+import Earth from './assets/earth/Earth.webp'
+import HowitzerTower from './assets/earth/Howitzer_Tower.webp'
+import GeyserTower from './assets/earth/Geyser_Tower.webp'
+import QuakeTower from './assets/earth/Quake_Tower.webp'
+import MoneyTower from './assets/earth/Money_Tower.webp'
+import MuckTower from './assets/earth/Muck_Tower.webp'
+import ShredderTower from './assets/earth/Shredder_Tower.webp'
+import NuclearReactorTower from './assets/earth/NuclearReactor_Tower.webp'
+import GravityCannonTower from './assets/earth/GravityCannon_Tower.webp'
+
+export type ElementName = 'light' | 'dark' | 'water' | 'fire' | 'nature' | 'earth'
+export const elementNames: ElementName[] = ['light', 'dark', 'water', 'fire', 'nature', 'earth']
+
+export type Range = 750 | 900 | 1150 | 1500
+export const ranges: Range[] = [750, 900, 1150, 1500]
+
+export type Support = 'buff' | 'slow' | 'amplify'
+export const supports: Support[] = ['buff', 'slow', 'amplify']
+
+export type Elements = ElementName[]
+
+export const getElementColor = (element: ElementName) =>
+	element === 'light'
+		? 'bg-yellow-100'
+		: element === 'dark'
+		? 'bg-purple-800'
+		: element === 'water'
+		? 'bg-blue-500'
+		: element === 'fire'
+		? 'bg-red-500'
+		: element === 'nature'
+		? 'bg-green-500'
+		: 'bg-yellow-800'
+
+export const getBackgroundColor = (element: ElementName) =>
+	element === 'light'
+		? 'bg-yellow-100/20'
+		: element === 'dark'
+		? 'bg-purple-800/20'
+		: element === 'water'
+		? 'bg-blue-500/20'
+		: element === 'fire'
+		? 'bg-red-500/20'
+		: element === 'nature'
+		? 'bg-green-500/20'
+		: 'bg-yellow-800/20'
+
+export type Tower = {
+	name: string
+	elements: ElementName[]
+	image: string
+	support?: Support
+	range: Range
+}
+
+export const towers: Record<ElementName, Tower[]> = {
+	light: [
+		{
+			name: 'Light',
+			elements: ['light'],
+			image: Light,
+			range: 1500,
+		},
+		{
+			name: 'Atom',
+			elements: ['light', 'earth'],
+			image: AtomTower,
+			range: 900,
+		},
+		{
+			name: 'Lightning',
+			elements: ['light', 'fire'],
+			image: LightningTower,
+			range: 900,
+		},
+		{
+			name: 'Trickery',
+			elements: ['light', 'dark'],
+			image: TrickeryTower,
+			range: 900,
+			support: 'buff',
+		},
+		{
+			name: 'Astral',
+			elements: ['light', 'dark', 'water'],
+			image: AstralTower,
+			range: 1500,
+		},
+		{
+			name: 'Laser',
+			elements: ['light', 'dark', 'earth'],
+			image: LaserTower,
+			range: 900,
+		},
+		{
+			name: 'Nova',
+			elements: ['light', 'fire', 'nature'],
+			image: NovaTower,
+			range: 900,
+			support: 'slow',
+		},
+		{
+			name: 'Railgun',
+			elements: ['light', 'water', 'fire', 'earth'],
+			image: RailgunTower,
+			range: 750,
+		},
+		{
+			name: 'Singularity',
+			elements: ['light', 'dark', 'water', 'fire'],
+			image: SingularityTower,
+			range: 900,
+		},
+	],
+	dark: [
+		{
+			name: 'Dark',
+			elements: ['dark'],
+			image: Darkness,
+			range: 1500,
+		},
+
+		{
+			name: 'Poison',
+			elements: ['dark', 'water'],
+			image: PoisonTower,
+			range: 900,
+		},
+		{
+			name: 'Disease',
+			elements: ['dark', 'nature'],
+			image: DiseaseTower,
+			range: 900,
+		},
+
+		{
+			name: 'Runic',
+			elements: ['dark', 'fire', 'light'],
+			image: RunicTower,
+			range: 1150,
+		},
+		{
+			name: 'Ethereal',
+			elements: ['dark', 'nature', 'light'],
+			image: EtherealTower,
+			range: 1500,
+		},
+		{
+			name: 'Jinx',
+			elements: ['dark', 'fire', 'nature'],
+			image: JinxTower,
+			range: 900,
+			support: 'amplify',
+		},
+
+		{
+			name: 'Plague',
+			elements: ['dark', 'water', 'nature', 'earth'],
+			image: PlagueTower,
+			range: 750,
+		},
+		{
+			name: 'Doom',
+			elements: ['dark', 'fire', 'nature', 'earth'],
+			image: DoomTower,
+			range: 900,
+		},
+		{
+			name: 'Phantom Zone',
+			elements: ['dark', 'water', 'earth', 'light'],
+			image: PhantomZoneTower,
+			range: 900,
+		},
+	],
+	water: [
+		{
+			name: 'Water',
+			elements: ['water'],
+			image: Water,
+			range: 900,
+		},
+
+		{
+			name: 'Vapor',
+			elements: ['water', 'fire'],
+			image: VaporTower,
+			range: 900,
+		},
+		{
+			name: 'Ice',
+			elements: ['water', 'light'],
+			image: IceTower,
+			range: 900,
+		},
+		{
+			name: 'Well',
+			elements: ['water', 'nature'],
+			image: WellTower,
+			range: 900,
+			support: 'buff',
+		},
+
+		{
+			name: 'Flooding',
+			elements: ['water', 'nature', 'dark'],
+			image: FloodingTower,
+			range: 1150,
+		},
+		{
+			name: 'Wisp',
+			elements: ['water', 'nature', 'light'],
+			image: WispTower,
+			range: 900,
+		},
+		{
+			name: 'Windstorm',
+			elements: ['water', 'fire', 'light'],
+			image: WindstormTower,
+			range: 1150,
+			support: 'slow',
+		},
+		{
+			name: 'Polar',
+			elements: ['water', 'earth', 'light'],
+			image: PolarTower,
+			range: 900,
+			support: 'amplify',
+		},
+
+		{
+			name: 'Tsunami',
+			elements: ['water', 'fire', 'nature', 'earth'],
+			image: TsunamiTower,
+			range: 1150,
+		},
+		{
+			name: 'Crystal Spire',
+			elements: ['water', 'fire', 'earth', 'dark'],
+			image: CrystalSpireTower,
+			range: 1150,
+		},
+	],
+	fire: [
+		{
+			name: 'Fire',
+			elements: ['fire'],
+			image: Fire,
+			range: 750,
+		},
+		{
+			name: 'Solar',
+			elements: ['fire', 'nature'],
+			image: SolarTower,
+			range: 750,
+		},
+		{
+			name: 'Infernal',
+			elements: ['fire', 'dark'],
+			image: InfernalTower,
+			range: 750,
+		},
+		{
+			name: 'Blacksmith',
+			elements: ['fire', 'earth'],
+			image: BlacksmithTower,
+			range: 900,
+			support: 'buff',
+		},
+		{
+			name: 'Flamethrower',
+			elements: ['fire', 'earth', 'dark'],
+			image: FlamethrowerTower,
+			range: 750,
+		},
+		{
+			name: 'Haste',
+			elements: ['fire', 'earth', 'water'],
+			image: HasteTower,
+			range: 900,
+		},
+		{
+			name: 'Corrosion',
+			elements: ['fire', 'dark', 'water'],
+			image: CorrosionTower,
+			range: 900,
+			support: 'amplify',
+		},
+		{
+			name: 'Obelisk',
+			elements: ['fire', 'nature', 'light', 'water'],
+			image: ObeliskTower,
+			range: 900,
+		},
+		{
+			name: 'Rage',
+			elements: ['fire', 'nature', 'earth', 'light'],
+			image: RageTower,
+			range: 900,
+		},
+	],
+	nature: [
+		{
+			name: 'Nature',
+			elements: ['nature'],
+			image: Nature,
+			range: 750,
+		},
+		{
+			name: 'Mushroom',
+			elements: ['nature', 'earth'],
+			image: MushroomTower,
+			range: 750,
+		},
+		{
+			name: 'Bloom',
+			elements: ['nature', 'light'],
+			image: BloomTower,
+			range: 750,
+		},
+		{
+			name: 'Impulse',
+			elements: ['nature', 'water', 'fire'],
+			image: ImpulseTower,
+			range: 1500,
+		},
+		{
+			name: 'Golem',
+			elements: ['nature', 'earth', 'water'],
+			image: GolemTower,
+			range: 1150,
+		},
+		{
+			name: 'Root',
+			elements: ['nature', 'earth', 'dark'],
+			image: RootTower,
+			range: 900,
+			support: 'slow',
+		},
+		{
+			name: 'Incantation',
+			elements: ['nature', 'earth', 'light'],
+			image: IncantationTower,
+			range: 900,
+			support: 'amplify',
+		},
+		{
+			name: 'Archdruid',
+			elements: ['nature', 'dark', 'water', 'fire'],
+			image: ArchdruidTower,
+			range: 900,
+		},
+		{
+			name: 'Tesla Tree',
+			elements: ['nature', 'light', 'dark', 'water'],
+			image: TeslaTreeTower,
+			range: 900,
+		},
+		{
+			name: 'Life Altar',
+			elements: ['nature', 'light', 'dark', 'fire'],
+			image: LifeAltarTower,
+			range: 900,
+		},
+	],
+	earth: [
+		{
+			name: 'Earth',
+			elements: ['earth'],
+			image: Earth,
+			range: 900,
+		},
+		{
+			name: 'Howitzer',
+			elements: ['earth', 'dark'],
+			image: HowitzerTower,
+			range: 1500,
+		},
+		{
+			name: 'Geyser',
+			elements: ['earth', 'water'],
+			image: GeyserTower,
+			range: 750,
+		},
+		{
+			name: 'Quake',
+			elements: ['earth', 'fire', 'nature'],
+			image: QuakeTower,
+			range: 750,
+		},
+		{
+			name: 'Money',
+			elements: ['earth', 'light', 'fire'],
+			image: MoneyTower,
+			range: 1150,
+		},
+		{
+			name: 'Muck',
+			elements: ['earth', 'dark', 'water'],
+			image: MuckTower,
+			range: 900,
+			support: 'slow',
+		},
+		{
+			name: 'Shredder',
+			elements: ['earth', 'light', 'dark', 'fire'],
+			image: ShredderTower,
+			range: 900,
+		},
+		{
+			name: 'Nuclear',
+			elements: ['earth', 'light', 'dark', 'nature'],
+			image: NuclearReactorTower,
+			range: 900,
+		},
+		{
+			name: 'Gravity Cannon',
+			elements: ['earth', 'light', 'water', 'nature'],
+			image: GravityCannonTower,
+			range: 1500,
+		},
+	],
+}
+
+export const getTowerParents = (tower: Tower) => {
+	return Object.values(towers)
+		.flat()
+		.filter((x) => x.elements.length === tower.elements.length - 1)
+		.filter((x) => x.elements.reduce((pass, el) => pass && tower.elements.includes(el), true))
+}
